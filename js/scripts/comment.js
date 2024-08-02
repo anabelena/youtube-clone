295d5cfa8a7b25307d3c2adfe3371f1585c93a27
anabelena <anabelen.aristah@gmail.com>
2024-08-01 20:38:33 -0700

    Haciendo dinamica la carga de imagenes en el grid.

---
+++ js/scripts/comment.js
@@ -0,0 +1,19 @@
+
+import { commentTemplate } from "../template/comment.js";
+import { commentData } from "../helpers/get-comment-data.js";
+
+const handleRenderComments = () => {
+  const commentsGrid = document.getElementById('comments-grid')
+
+  if (!commentsGrid) return
+
+  for (let i = 1; i <= 30; i++) {
+    const element = document.createElement('article')
+    const data = commentData()
+    element.classList.add('comment', 'flex')
+    element.innerHTML = commentTemplate(data)
+    commentsGrid.appendChild(element)
+  }
+}
+
+handleRenderComments()
